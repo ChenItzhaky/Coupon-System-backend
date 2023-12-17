9b70f9821d921a25f23e4f120a4446f0a0070702
חן <napoleon14@gmail.com>
2023-12-17 16:32:04 +0200

    Baby Steps

--- src/main/java/com/chen/couponys/jobs/ClearExpiredToken.java
+++ src/main/java/com/chen/couponys/jobs/ClearExpiredToken.java
@@ -5,12 +5,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 public class ClearExpiredToken {
     @Autowired
     private TokenService tokenService;
 
-    @Scheduled(fixedDelay = 1000*60)
+    @Scheduled(fixedDelay = 1000*60*30)
     public void clearTokes(){
         tokenService.clear();
     }
--- src/main/java/com/chen/couponys/jobs/dailyExpiredRemoval.java
+++ src/main/java/com/chen/couponys/jobs/dailyExpiredRemoval.java
@@ -10,7 +10,7 @@ import java.sql.Date;
 import java.time.LocalDate;
 import java.util.List;
 
-//@Component
+@Component
 public class dailyExpiredRemoval {
     @Autowired
     private CouponRepository couponRepository;
